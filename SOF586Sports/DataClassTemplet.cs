1d2c333b00403502749ed3395a00e18877911a68
sahithi <sahithin188@gmail.com>
2019-07-24 17:29:10 -0500

    Add project files.

---
+++ SOF586Sports/DataClassTemplet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SOFSports
+{
+    public class DataClassTemplate  
+    {
+
+    }
+}
