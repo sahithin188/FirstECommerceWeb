0b34f84cb46032a432b6e57b188d0ecda5a7ee7b
sahithi <sahithin188@gmail.com>
2019-07-25 00:16:01 -0500

    added models to sof 586

---
+++ SOF586Sports/wwwroot/Models/Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOFSports.wwwroot.Models
+{
+    public partial class Items
+    {
+        public Items()
+        {
+            PurchaseHistory = new HashSet<PurchaseHistory>();
+        }
+
+        public int ItemsId { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<PurchaseHistory> PurchaseHistory { get; set; }
+    }
+}
