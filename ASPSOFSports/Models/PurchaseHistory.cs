5a7299dc14fc28ede02d894c11e9b015f3e8d79e
Avinash.Chaparala <Avinash.Chaparala@target.com>
2019-07-26 13:06:12 -0500

    added ASP appliocation

---
+++ ASPSOFSports/Models/PurchaseHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPSOFSports.Models
+{
+    public partial class PurchaseHistory
+    {
+        public int PurchaseHistoryId { get; set; }
+        public int? ItemsId { get; set; }
+        public int? UserInfoId { get; set; }
+        public int? Quantity { get; set; }
+        public bool IsPurchased { get; set; }
+
+        public virtual Items Items { get; set; }
+        public virtual UserInfo UserInfo { get; set; }
+    }
+}
---
+++ ASPSOFSports/Models/ShoppingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPSOFSports.Models
+{
+    public class ShoppingModel
+    {
+       
+    }
+}
