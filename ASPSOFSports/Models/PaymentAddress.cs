c68264c7d3ee065eff17b911272aaf32a885b91e
sahithi <sahithin188@gmail.com>
2019-07-26 13:24:39 -0500

    database added

---
+++ ASPSOFSports/Models/PaymentAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPSOFSports.Models
+{
+    public partial class PaymentAddress
+    {
+        public int PaymentAddressId { get; set; }
+        public int? UserInfoId { get; set; }
+        public string Street { get; set; }
+        public string AptNo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+        public virtual UserInfo UserInfo { get; set; }
+    }
+}
