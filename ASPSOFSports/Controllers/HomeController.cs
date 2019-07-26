11cfbad02fe21ab8b2ce1cc370ceed1c4036a031
Avinash.Chaparala <Avinash.Chaparala@target.com>
2019-07-26 16:53:33 -0500

    updated code using enity framwork

--- ASPSOFSports/Controllers/HomeController.cs
+++ ASPSOFSports/Controllers/HomeController.cs
@@ -13,38 +13,42 @@ namespace ASPSOFSports.Controllers
 {
     public class HomeController : Controller
     {
+        public SOF586SportsContext db_context;
         public IConfiguration Configuration { get; }
         public string connectionString;
         public HomeController(IConfiguration configuration)
         {
             Configuration = configuration;
             connectionString = Configuration.GetConnectionString("SOFSports");
+            db_context = new SOF586SportsContext(Configuration);
         }
         public IActionResult Shopping()
         {
-            List<Items> itemlist = new List<Items>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                //SqlDataReader
-                connection.Open();
+            //List<Items> itemlist = new List<Items>();
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    //SqlDataReader
+            //    connection.Open();
 
-                string sql = "Select * From Items "; SqlCommand command = new SqlCommand(sql, connection);
-                using (SqlDataReader dataReader = command.ExecuteReader())
-                {
-                    while (dataReader.Read())
-                    {
-                        Items Item = new Items();
-                        Item.ItemsId = Convert.ToInt32(dataReader["ItemsId"]);
-                        Item.Name = Convert.ToString(dataReader["Name"]);
-                        Item.Price = Convert.ToDecimal(dataReader["Price"]);
-                        Item.Description = Convert.ToString(dataReader["Description"]);
+            //    string sql = "Select * From Items "; SqlCommand command = new SqlCommand(sql, connection);
+            //    using (SqlDataReader dataReader = command.ExecuteReader())
+            //    {
+            //        while (dataReader.Read())
+            //        {
+            //            Items Item = new Items();
+            //            Item.ItemsId = Convert.ToInt32(dataReader["ItemsId"]);
+            //            Item.Name = Convert.ToString(dataReader["Name"]);
+            //            Item.Price = Convert.ToDecimal(dataReader["Price"]);
+            //            Item.Description = Convert.ToString(dataReader["Description"]);
 
-                        itemlist.Add(Item);
-                    }
-                }
-                connection.Close();
-            }
-            return View(itemlist);
+            //            itemlist.Add(Item);
+            //        }
+            //    }
+            //    connection.Close();
+            //}
+
+            //return View(itemlist);
+             return View(db_context.Items);
             //return View();
         }
 
@@ -74,22 +78,50 @@ namespace ASPSOFSports.Controllers
         }
 
         [HttpPost]
-        public IActionResult Pay(Items Items)
+        public IActionResult Pay(Items Item)
         {
             if (ModelState.IsValid)
-            { 
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    string sql = $"Insert Into Items (ItemsId, Name, Price, PurchaseHistory) Values ('{Items.ItemsId}', '{ Items.Name}','{Items.Price}','{Items.PurchaseHistory}')";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        command.CommandType = CommandType.Text;
-                        connection.Open();
-                        command.ExecuteNonQuery();
-                        connection.Close();
-                    }
+            {
+                db_context.Items.Add(Item);
+                db_context.SaveChanges();
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    string sql = $"Insert Into Items (ItemsId, Name, Price, PurchaseHistory) Values ('{Items.ItemsId}', '{ Items.Name}','{Items.Price}','{Items.PurchaseHistory}')";
+                //    using (SqlCommand command = new SqlCommand(sql, connection))
+                //    {
+                //        command.CommandType = CommandType.Text;
+                //        connection.Open();
+                //        command.ExecuteNonQuery();
+                //        connection.Close();
+                //    }
                     return RedirectToAction("Payment");
-                }
+                //}
+            }
+            else
+                return View();
+        }
+
+        public IActionResult AddtoCart(Items Items)
+        {
+            PurchaseHistory ph = new PurchaseHistory();
+            ph.ItemsId = Items.ItemsId;
+            ph.UserInfoId = 1;
+            ph.Quantity = 1;
+            ph.IsPurchased = false;
+            if (ModelState.IsValid)
+            {
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    string sql = $"Insert Into PurchaseHistory (PurchaseHistoryId, ItemsId, UserInfoId, Quantity,IsPurchased) Values ('{Items.ItemsId}', '{ Items.Name}','{Items.Price}','{Items.PurchaseHistory}')";
+                //    using (SqlCommand command = new SqlCommand(sql, connection))
+                //    {
+                //        command.CommandType = CommandType.Text;
+                //        connection.Open();
+                //        command.ExecuteNonQuery();
+                //        connection.Close();
+                //    }
+                    return RedirectToAction("Payment");
+                //}
             }
             else
                 return View();
